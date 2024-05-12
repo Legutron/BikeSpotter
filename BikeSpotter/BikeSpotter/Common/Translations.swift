ac017bc2103b2615963867df82496875ecff336e
Jakub Legut <jakub-legut@wp.pl>
2024-05-12 18:42:27 +0200

    handle translations

---
+++ BikeSpotter/BikeSpotter/Common/Translations.swift
@@ -0,0 +1,13 @@
+//
+//  Translations.swift
+//  BikeSpotter
+//
+//  Created by Jakub Legut on 12/05/2024.
+//
+
+import Foundation
+
+enum Translations {
+	static let bikesValueLabel = NSLocalizedString("bikes_value_label", comment: "")
+	static let placesValueLabel = NSLocalizedString("places_value_label", comment: "")
+}
