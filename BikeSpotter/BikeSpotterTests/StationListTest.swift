8c14191eaabefddeac1ce24d6867aecf9d1a7d30
Jakub Legut <jakub-legut@wp.pl>
2024-05-20 12:44:54 +0200

    some work

---
+++ BikeSpotter/BikeSpotterTests/StationListTest.swift
@@ -0,0 +1,33 @@
+//
+//  StationListTest.swift
+//  BikeSpotterTests
+//
+//  Created by Jakub Legut on 17/05/2024.
+//
+
+@testable import BikeSpotter
+import XCTest
+
+final class BikeSpotterListTests: XCTestCase {
+	
+	override func setUpWithError() throws {
+		continueAfterFailure = false
+	}
+
+	func testExample() throws {
+//		let app = XCUIApplication()
+//		app.launch()
+//		
+//		let vm = StationListViewModel()
+//		vm.onLoad()
+		
+	}
+
+	func testPerformanceExample() throws {
+		// This is an example of a performance test case.
+		self.measure {
+			// Put the code you want to measure the time of here.
+		}
+	}
+
+}
