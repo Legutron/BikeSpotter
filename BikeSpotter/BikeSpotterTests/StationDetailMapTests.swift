39b172ff1c7a13505462574d8c083e785c99c623
Jakub Legut <jakub-legut@wp.pl>
2024-05-21 14:32:49 +0200

    add tests

---
+++ BikeSpotter/BikeSpotterTests/StationDetailMapTests.swift
@@ -0,0 +1,16 @@
+//
+//  StationDetailMapTests.swift
+//  BikeSpotterTests
+//
+//  Created by Jakub Legut on 21/05/2024.
+//
+
+@testable import BikeSpotter
+import XCTest
+
+final class StationDetailMapTests: XCTestCase {
+	
+	override func setUpWithError() throws {
+		continueAfterFailure = false
+	}
+}
